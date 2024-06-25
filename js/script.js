eb57f75df955744794a3a770b98a37dfd414167f
b4bishal <138556411+b4bishal@users.noreply.github.com>
2024-06-25 17:45:37 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ onload = () => {
 };
 let a = document.getElementById("btnno");
 function yess(){
-  alert("I promise to stand you  in any situation.💗💗💗 Bacha Vayo timi lai mero");
+  alert("I promise to cherish you every day, to support you through life's joys and challenges, and to build our future together with love and understanding. Today marks the beginning of our journey, and I vow to always be by your side.💗💗💗 Bacha Vayo timi lai mero");
 }
 function noo(){
   a.style.display='none';
